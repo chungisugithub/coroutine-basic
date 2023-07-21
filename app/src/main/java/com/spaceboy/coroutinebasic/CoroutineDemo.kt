fd5086b44e1eefef9dab5aea96c9ddc35bd51072
spaceboy <chungisu@gmail.com>
2023-07-22 07:35:28 +0900

    repeat(50) 사용 50개의 코루틴 생성

--- app/src/main/java/com/spaceboy/coroutinebasic/CoroutineDemo.kt
+++ app/src/main/java/com/spaceboy/coroutinebasic/CoroutineDemo.kt
@@ -1,37 +1,19 @@
 package com.spaceboy.coroutinebasic
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 fun main() {
 
-    GlobalScope.launch {
-        launch {
-            task2()
+    repeat(50) {
+        CoroutineScope(Dispatchers.Default).launch {
             println(Thread.currentThread().name)
         }
     }
 
-    GlobalScope.launch {
-        launch {
-            task1()
-            println(Thread.currentThread().name)
-        }
-    }
-
-
-    Thread.sleep(2000L)
-    println(Thread.currentThread().name)
-
+    Thread.sleep(1000L)
 }
 
-suspend fun task1() {
-
-    print("Hello ")
-
-}
-
-suspend fun task2() {
-    print("World!")
-}
