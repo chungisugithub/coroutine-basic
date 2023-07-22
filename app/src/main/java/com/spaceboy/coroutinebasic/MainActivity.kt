cc51d60d6293f35bc5d91d19f187de83a33c5d83
chungisugithub <chungisu@gmail.com>
2023-07-23 02:38:57 +0900

    CoroutineScope vs. coroutineScope

--- app/src/main/java/com/spaceboy/coroutinebasic/MainActivity.kt
+++ app/src/main/java/com/spaceboy/coroutinebasic/MainActivity.kt
@@ -2,6 +2,7 @@ package com.spaceboy.coroutinebasic
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,11 +16,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.spaceboy.coroutinebasic.ui.theme.CoroutineBasicTheme
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    val scope = MainScope()
+    val scope = CoroutineScope(CoroutineName("MyScope"))
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,16 +34,57 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     LaunchedEffect(key1 = true) {
-                        scope.launch {
-                            Log.d("CoroutineName", this.coroutineContext.toString())
-                            Log.d("CoroutineName", Thread.currentThread().name)
-                        }
+//                        scope.launch {
+//                            Log.d("CoroutineName", this.coroutineContext.toString())
+//                            Log.d("CoroutineName", Thread.currentThread().name)
+//                        }
+                        /*
+                        https://stackoverflow.com/questions/59368838/difference-between-coroutinescope-and-coroutinescope-in-kotlin
+                        CoroutineScope vs. coroutineScope
+                        Unstructured vs. Structured
+                         */
+//                        val result = downloadUserData1() // Output : Result : 0
+                        val result = downloadUserData2() // Output : Result : 100
+                        Toast.makeText(applicationContext, "Result : $result", Toast.LENGTH_LONG).show()
+
                     }
 
                     Greeting("Android")
                 }
             }
         }
+    }
+
+    //Unstructured Concurrency
+    private suspend fun downloadUserData1(): Int {
+        var result = 0
+        // Here, we use CoroutineScope (Capital C version) which will start a new scope and
+        // launch coroutine in new scope Dispatchers.IO, Not In Parent Scope which is Dispatchers.Main
+        // Thus, this function would directly return without waiting for loop completion and will return 0
+        CoroutineScope(Dispatchers.IO).launch {
+            for (i in 0 until 100) {
+                kotlinx.coroutines.delay(10)
+                result++
+            }
+        }
+        return result
+        //Output : Result : 0
+    }
+
+    //Structured Concurrency (Recommended)
+    private suspend fun downloadUserData2(): Int {
+        var result = 0
+        // By using coroutineScope (Smaller c version) below, we ensure that this coroutine would execute in the
+        // parent/caller coroutine's scope, so it would make sure that the for loop would complete
+        // before returning from this suspended function. This will return 20000 properly
+        coroutineScope {
+            for (i in 0 until 100) {
+                kotlinx.coroutines.delay(10)
+                result++
+            }
+        }
+        return result
+        //Output : Result : 100
     }
 }
 
