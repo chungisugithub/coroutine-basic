3ff9e5ba4bb0f264a55c4b4c622c16d4f48b8f82
chungisugithub <chungisu@gmail.com>
2023-07-23 00:27:06 +0900

    에러 - scope.launch Calls to launch should happen inside a LaunchedEffect and not composition

--- app/src/main/java/com/spaceboy/coroutinebasic/MainActivity.kt
+++ app/src/main/java/com/spaceboy/coroutinebasic/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.spaceboy.coroutinebasic
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,8 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.spaceboy.coroutinebasic.ui.theme.CoroutineBasicTheme
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    val scope = CoroutineScope(CoroutineName("MyScope"))
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,6 +27,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    scope.launch {
+                        Log.d("Coroutine", this.coroutineContext.toString())
+                    }
                     Greeting("Android")
                 }
             }
